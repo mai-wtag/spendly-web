988a5b125832584f5eba444669e6e9bf8a1ebd8e
Md. Mainul Islam <mainul.islam@welldev.io>
2025-09-30 14:30:26 +0600

    Added validator library with separate validation file

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
     alias: {
       'components': path.resolve(__dirname, './src/components'),
       'pages': path.resolve(__dirname, './src/pages'),
-      'assets': path.resolve(__dirname, './src/assets')
+      'assets': path.resolve(__dirname, './src/assets'),
+      'utils': path.resolve(__dirname, './src/utils')
     }
   }
 })
